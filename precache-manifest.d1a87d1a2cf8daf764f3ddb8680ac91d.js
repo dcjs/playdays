7f479beba6076001ee7d731465ddc5d090d60e75
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-10-17 15:22:15 -0400

    Updates

---
+++ precache-manifest.d1a87d1a2cf8daf764f3ddb8680ac91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b997a88499081ff01beb7c25abec73a",
+    "url": "/playdays/index.html"
+  },
+  {
+    "revision": "847d41fa8c8966ebc456",
+    "url": "/playdays/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ad7d9f9f06743ea3fe7f",
+    "url": "/playdays/static/js/2.32d974c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/playdays/static/js/2.32d974c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847d41fa8c8966ebc456",
+    "url": "/playdays/static/js/main.50da8612.chunk.js"
+  },
+  {
+    "revision": "1aed18ef15d4c4759428",
+    "url": "/playdays/static/js/runtime-main.a3c04428.js"
+  }
+]);
