b6a9566213f1db282932313cf7ebae65f1df4d4f
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-10-17 14:40:12 -0400

    Add site with create-react-app

---
+++ site/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import About from './About';
+import Resources from './Resources';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Resources />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
